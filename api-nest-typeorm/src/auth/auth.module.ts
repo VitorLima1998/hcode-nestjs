aa4ac538ff9c8d71548ff64c8d88bfb192b538d2
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-12 16:41:55 -0400

    Refactor imports in auth module

--- api-nest-typeorm/src/auth/auth.module.ts
+++ api-nest-typeorm/src/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
-import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
-import { FileModule } from 'src/file/file.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from 'src/user/entities/user.entity';
 import { config } from 'dotenv';
+import { UserEntity } from '../user/entities/user.entity';
+import { UserModule } from '../user/user.module';
+import { FileModule } from '../file/file.module';
 config();
 
 @Module({
