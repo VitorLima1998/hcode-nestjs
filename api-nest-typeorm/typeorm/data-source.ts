2c3cc0e854c22ed6b21180e6ec9bec2bf802e14d
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-26 16:36:58 -0400

    Update data source configuration

--- api-nest-typeorm/typeorm/data-source.ts
+++ api-nest-typeorm/typeorm/data-source.ts
@@ -1,6 +1,17 @@
 import { DataSource } from 'typeorm';
-import { config } from 'dotenv';
-config();
+import * as dotenv from 'dotenv';
+
+const envFilePath = process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+dotenv.config({
+    path: envFilePath,
+});
+
+// console.log('Environment:', process.env.NODE_ENV);
+// console.log('DB_HOST:', process.env.DB_HOST);
+// console.log('DB_PORT:', process.env.DB_PORT);
+// console.log('DB_ROOT_USERNAME:', process.env.DB_ROOT_USERNAME);
+// console.log('DB_ROOT_PASSWORD:', process.env.DB_ROOT_PASSWORD);
+// console.log('DATABASE:', process.env.DATABASE);
 
 const dataSource = new DataSource({
     type: 'mysql',
