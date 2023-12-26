2de2b7ce36c96fd760c316a59493b82b71c9d35f
VitorLima1998 <vitor.xlr8@gmail.com>
2023-12-26 16:36:30 -0400

    Remove console.log statements from UserIdCheckMiddleware

--- api-nest-typeorm/src/middlewares/user-id-check.middleware.ts
+++ api-nest-typeorm/src/middlewares/user-id-check.middleware.ts
@@ -5,13 +5,11 @@ export class UserIdCheckMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         const uuidRegex =
             /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
-        console.log(`UserCheckMiddleware before`);
 
         if (!uuidRegex.test(req.params.id)) {
             throw new BadRequestException('Invalid UUID');
         }
 
-        console.log(`UserCheckMiddleware after`);
         next();
     }
 }
